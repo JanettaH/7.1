b280ed598981882148bc1da60aa183aca0be9632
JanettaH <janetta.huoponen@gmail.com>
2019-07-09 18:03:31 +0300

    7.3

--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView text;
+    EditText textField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //this.testFunction();
         text = (TextView) findViewById(R.id.textView2);
+        textField = (EditText) findViewById(R.id.editText);
 
     }
 
     public void testFunction(View v) {
         System.out.println("Hello World!");
-        text.setText("Hello World!");
+        String newText = textField.getText().toString();
+        text.setText(newText);
     }
 }
